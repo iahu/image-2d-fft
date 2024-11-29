f962adcafd594c68e643ac1df7709c2095df6dce
i18u <iahu1988@gmail.com>
2024-11-29 17:41:02 +0800

    feat: gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path, { dirname } from 'node:path';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/image-2d-fft/',
 
   resolve: {
     alias: {
