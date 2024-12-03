bfd22a544464e0c942afb0dd896bba9637e30ba2
i18u <iahu1988@gmail.com>
2024-12-03 09:50:10 +0800

    doc: update index page docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,18 @@ import { Link } from 'react-router';
 
 export const IndexPage = () => {
   return (
-    <div className="index-page">
+    <div className="index-page p-20">
+      <h1>FFT online demo</h1>
       <ul>
         <li>
-          <Link to="/image-2d-fft">image-2d-fft</Link>
+          <Link to="/wave">periodic wave fft</Link>
         </li>
         <li>
-          <Link to="/wave">wave</Link>
+          <Link to="/image">image 2d fft</Link>
         </li>
       </ul>
+
+      <a href="https://github.com/iahu/image-2d-fft">github repository</a>
     </div>
   );
 };
